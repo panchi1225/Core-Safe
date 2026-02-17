6eecd277ffc30f08d02dbf6d29fa68509415d164
panchi1225 <paaaaanchi1225@gmail.com>
2026-02-18 07:18:45 +0900

    パス設定を修正して公開

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ★修正箇所：全部小文字ではなく、URLに合わせて大文字にします
+      base: "/Core-Safe/", 
+
       server: {
         port: 3000,
         host: '0.0.0.0',
